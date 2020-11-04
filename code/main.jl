f8bc7e138b1e9e52f6c5b4b30e19dd175b59763c
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-11-04 12:59:40 -0500

    in-place imputation

--- code/main.jl
+++ code/main.jl
@@ -21,6 +21,30 @@ for record in eachrow(virionette)
     N[record.virus_genus, record.host_species] = true
 end
 
+include("./code/src/functions.jl")
+
+function impute!(output::T1, tmp::T1, data::T2, template::T3, position; r=2, maxiter=50, tolerance=1e-2) where {T1 <: QuantitativeNetwork, T2 <: AbstractEcologicalNetwork, T3 <: ProbabilisticNetwork}
+    tmp.A[position] = template.A[position]
+    Δ = 1.0
+    iter = 1
+    while Δ > tolerance
+        iter += 1
+        A = lowrank(tmp; r=r)
+        Δ = abs(A[position] - N[position])
+        tmp.A[position] = A.A[position]
+        iter ≥ maxiter && break
+    end
+    output.A[position] = tmp.A[position]
+    tmp.A[position] = data.A[position] # RESET!
+end
+
+T = BipartiteQuantitativeNetwork(float.(copy(N.A)), EcologicalNetworks.species_objects(N)...)
+O = copy(T)
 R = EcologicalNetworks.linearfilter(N; α=[0.0, 1.0, 1.0, 1.0])
 
-svd(R.A)
+@time for i in eachindex(R.A)
+    if !(N.A[i])
+        @info i
+        @time impute!(O, T, N, R, i)
+    end
+end
--- code/src/functions.jl
+++ code/src/functions.jl
@@ -9,8 +9,7 @@ end
 function lowrank(N::T; r::Integer=1) where {T <: AbstractEcologicalNetwork}
     r >= rank(N) && throw(ArgumentError("r ($(r)) is larger than the rank ($(rank(N))) of the network"))
     factor = svd(N)
-    S = factor.S
-    S[(r+1):end] .= zero(eltype(S))
+    factor.S[(r+1):end] .= zero(eltype(factor.S))
     _rt = T <: AbstractUnipartiteNetwork ? UnipartiteQuantitativeNetwork : BipartiteQuantitativeNetwork
-    return _rt(factor.U * Diagonal(S) * factor.Vt, EcologicalNetworks.species_objects(N)...)
+    return _rt(factor.U * Diagonal(factor.S) * factor.Vt, EcologicalNetworks.species_objects(N)...)
 end
