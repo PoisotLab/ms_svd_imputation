9bd0e0099ba4eca7b6c66e50330a3d4607dd80f6
Timothée Poisot <tpoisot@pop-os.localdomain>
2020-11-04 21:05:25 -0500

    move impute! to its own function

--- code/src/functions.jl
+++ code/src/functions.jl
@@ -13,3 +13,19 @@ function lowrank(N::T; r::Integer=1) where {T <: AbstractEcologicalNetwork}
     _rt = T <: AbstractUnipartiteNetwork ? UnipartiteQuantitativeNetwork : BipartiteQuantitativeNetwork
     return _rt(factor.U * Diagonal(factor.S) * factor.Vt, EcologicalNetworks.species_objects(N)...)
 end
+
+function impute!(output::T1, tmp::T1, template::T2, position; r=2, maxiter=50, tolerance=1e-2) where {T1 <: QuantitativeNetwork, T2 <: ProbabilisticNetwork}
+    orig = tmp.A[position]
+    tmp.A[position] = template.A[position]
+    Δ = 1.0
+    iter = 1
+    while Δ > tolerance
+        iter += 1
+        A = lowrank(tmp; r=r)
+        Δ = abs(A[position] - tmp[position])
+        tmp.A[position] = A.A[position]
+        iter ≥ maxiter && break
+    end
+    output.A[position] = tmp.A[position] # STORE!
+    tmp.A[position] = orig # RESET!
+end
