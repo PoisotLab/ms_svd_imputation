39f75ca2fefb728a97a1f447ca7167adaaa74421
Timothée Poisot <tpoisot@pop-os.localdomain>
2020-11-04 21:54:47 -0500

    update with a figure (not quite there yet)

--- code/main.jl
+++ code/main.jl
@@ -8,6 +8,7 @@ begin
     using LinearAlgebra, Statistics
     using Plots
     using ProgressMeter
+    using EcologicalNetworksPlots
 end
 
 # Read the data
@@ -36,6 +37,7 @@ end
 
 predictions = filter(i -> !(N[i.from, i.to]), interactions(O))
 sort!(predictions, by=(x) -> x.strength, rev=true)
+predictions = filter(x -> x.strength > R[x.from, x.to], predictions)
 plot(
     [x.strength for x in predictions],
     c=:grey, lab="",
@@ -43,3 +45,26 @@ plot(
     frame = :origin,
     xlab = "Rank", ylab="Score"
     )
+
+# Plot of additional predictions
+I = initial(RandomInitialLayout, N)
+@showprogress for step in 1:1000
+  position!(ForceDirectedLayout(0.005), I, N)
+end
+plot(I, N, aspectratio=1)
+scatter!(I, N, bipartite=true, nodesize=degree(N))
+
+core3 = collect(keys(filter(p -> p.second >= 3, degree(N))))
+Ncore = simplify(N[core3∩species(N; dims=1), core3∩species(N; dims=2)])
+
+plot(I, Ncore)
+scatter!(I, Ncore, nodesize=degree(N), bipartite=true)
+
+K = BipartiteNetwork(zeros(Bool, size(N)), EcologicalNetworks.species_objects(N)...)
+for pre in predictions[1:100]
+    K[pre.from, pre.to] = true
+end
+simplify!(K)
+
+plot!(I, K, lc=:red)
+scatter!(I, N, nodesize=degree(N), bipartite=true, mf=:white)
